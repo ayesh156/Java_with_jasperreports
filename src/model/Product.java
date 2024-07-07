d2d02e024087c6fbe1c67b0e13cda0d810b100db
= <=>
2024-07-07 23:08:10 +0530

    Initial Commit

---
+++ src/model/Product.java
@@ -0,0 +1,42 @@
+
+package model;
+
+public class Product {
+    
+    private String x;
+    private String y;
+    private String z;
+    
+    public Product(){
+        
+    }
+
+    public String getX() {
+        return x;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public String getY() {
+        return y;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
+
+    public String getZ() {
+        return z;
+    }
+
+    public void setZ(String z) {
+        this.z = z;
+    }
+
+    
+    
+    
+    
+}
